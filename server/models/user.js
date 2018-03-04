361b2348002b2c2c1562425a065bb2f319496953
steve <bashcandicoot@gmail.com>
2018-03-04 14:35:48 +0000

    updated user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,26 +1,31 @@
-const Sequelize = require('sequelize');
-
-const User = Sequelize.define('user', {
-  firstName: {
-    type: Sequelize.STRING,
-  },
-  lastName: {
-    type: Sequelize.STRING,
-  },
-  email: {
-    type: Sequelize.STRING,
-    unique: true,
-  },
-  dateOfBirth: {
-    type: Sequelize.DATE,
-  },
-  type: {
-    // type: ['SUPPLIER', 'TENANT'],
-  },
-  phone: {
-    type: Sequelize.STRING,
-    unique: true,
-  },
-});
-
-module.exports = User;
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    dateOfBirth: {
+      type: DataTypes.DATE,
+    },
+    phone: {
+      type: DataTypes.STRING,
+    },
+  }, {});
+  User.associate = (models) => {
+    console.log(models);
+    // associations can be defined here
+  };
+  return User;
+};
