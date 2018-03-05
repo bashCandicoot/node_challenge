20f056d17aa9d5394c1fed3916a79c3d3039a65b
steve <bashcandicoot@gmail.com>
2018-03-05 19:00:47 +0000

    suppliers route returns all suppliers successfully

--- server/routes/suppliers/all.js
+++ server/routes/suppliers/all.js
@@ -1,4 +1,6 @@
+const db = require('../../models/index');
+
 module.exports = (req, res) => {
-  // const allSuppliers = ;
-  // res.status(200).json({ allSuppliers });
+  db.sequelize.models.suppliers.findAll()
+    .then(suppliers => res.status(200).json({ suppliers }));
 };
