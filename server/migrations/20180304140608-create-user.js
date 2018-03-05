a4667c6621ab7b23d39efc911eba1a4d1e1fc46c
steve <bashcandicoot@gmail.com>
2018-03-05 17:21:58 +0000

    removed commented duplicate line

--- server/migrations/20180304140608-create-user.js
+++ server/migrations/20180304140608-create-user.js
@@ -36,6 +36,5 @@ module.exports = {
       type: Sequelize.DATE,
     },
   }),
-  // down: (queryInterface, Sequelize) => queryInterface.dropTable('Users'),
   down: queryInterface => queryInterface.dropTable('users'),
 };
