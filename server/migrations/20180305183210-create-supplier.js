2b5cc869ea8c76091090c7543898abe9d9e72fae
steve <bashcandicoot@gmail.com>
2018-03-05 19:00:02 +0000

    create supliers table migration

---
+++ server/migrations/20180305183210-create-supplier.js
@@ -0,0 +1,38 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('suppliers', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    name: {
+      type: Sequelize.STRING,
+    },
+    type: {
+      type: Sequelize.ENUM,
+      values: ['PRIVATE', 'AGENCY'],
+    },
+    signedTerms: {
+      type: Sequelize.BOOLEAN,
+    },
+    vatNumber: {
+      type: Sequelize.STRING,
+    },
+    addressId: {
+      type: Sequelize.INTEGER,
+    },
+    masterUser: {
+      type: Sequelize.INTEGER,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: queryInterface => queryInterface.dropTable('suppliers'),
+};
